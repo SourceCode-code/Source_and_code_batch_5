a3d08f6702865c9ff0f79f8e97aa3c6cdd21f5e3
SourceCode-code <siddhantsourceandcode@gmail.com>
2026-03-16 22:13:25 +0530

    introduction

---
+++ JAVASCRIPT/JAVASCRIPT_SESSION/01_Introduction.js
@@ -0,0 +1,37 @@
+// INTRODUCTION TO JAVASCRIPT
+
+// Javascript is verstaile programming langauge primarilyo used for web development,
+// it allows the user or deveopler to create dynamic web pages which will have a interactive response 
+
+
+
+//----> what is web page 
+
+// any webpage on internet today is made form 3 things 
+
+//HTML ->html is provide structure to the page used to display content 
+//CSS-> css will enable the deveopler to style his webpage 
+//JAVASCRIPT-> js is the brain of the web page ( it allows web page to interact with the user)
+// js maniplaute the html and css to achivce this interaction 
+
+
+// 
+
+// 1 why to learn to js as testor 
+// 2 how to code in js --> 
+// 3 how any to run any file in js 
+ //standard process 
+
+// 1 select the file that you want to run
+//2 right click on that file 
+// 3 select open with inetrgated terminal
+// 4 a terminal will be open below 
+// 5 enter a command in terminal --> node filename.js 
+
+//node filename.js   --> this will run your js file 
+
+
+// if you have to print or get any output on the terminal 
+console.log("hello world")
+
+

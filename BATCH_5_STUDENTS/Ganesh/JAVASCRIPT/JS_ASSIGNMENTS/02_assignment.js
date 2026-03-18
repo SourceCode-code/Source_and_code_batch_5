8363d53bd151c508a3bf773388b2cc058955a731
Ganesh <gdhage86@gmail.com>
2026-03-18 21:10:44 +0530

    Assignment 02 submission

---
+++ BATCH_5_STUDENTS/Ganesh/JAVASCRIPT/JS_ASSIGNMENTS/02_assignment.js
@@ -0,0 +1,47 @@
+// Write a single line comment which says, comments can make code readable
+// Comments can make code readable
+
+
+// Write another single comment which says, Welcome to 30DaysOfJavaScript
+// Welcome to 30DaysOfJavaScript
+
+
+// Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+/*
+comments can make code readable,
+easy to reuse and informative
+*/
+
+
+// Declare four variables without assigning values
+let variable1
+let variable2
+let variable3
+let variable4
+
+
+// Declare four variables with assigned value
+let variable5 = 5
+let variable6 = 3.14
+let variable7 = 0
+let variable8 = "address"
+
+
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+let firstName = "Ganesh"
+let lastName = "Dhage"
+let maritalStatus = "single"
+let country = "India"
+let age = "29"
+
+
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+let FirstName ="Ganesh" ; let LastName ="Dhage" ; let  MaritalStatus = "single" ; let Country = "India"; let Age = "29" ;
+//console.log(FirstName,LastName,MaritalStatus,Country,Age)
+
+
+// Declare two variables myAge and yourAge and assign them initial values and log to the console.
+var myAge = 29
+var yourAge = 30
+console.log(myAge)
+console.log(yourAge) 

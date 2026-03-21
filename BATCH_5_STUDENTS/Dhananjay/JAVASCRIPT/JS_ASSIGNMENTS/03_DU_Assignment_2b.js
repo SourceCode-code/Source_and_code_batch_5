0b4e036a3b81907641d44ce0c047257b3edac815
Dhananjay Ugwekar <dhananjayugwekar.work@gmil.com>
2026-03-21 19:58:33 +0530

    03_DU_Assignmet_2b

---
+++ BATCH_5_STUDENTS/Dhananjay/JAVASCRIPT/JS_ASSIGNMENTS/03_DU_Assignment_2b.js
@@ -0,0 +1,36 @@
+//Practice - 21.03.2026
+
+let weight = "29.49"
+
+console.log(Math.floor(weight))
+
+console.log(Math.ceil(weight))
+
+console.log(Math.round(weight))
+
+// num.toFixed() --> It convert the decimail values in the provided notaion
+
+let AvailableBal = 10.54984962263
+
+console.log(AvailableBal.toFixed(2))
+
+//Math.random() --> Generate random number 0 to 0.999999
+
+console.log(Math.random())
+
+let Random100 = Math.random()*100
+console.log(Random100)
+
+console.log(Math.floor(Math.random()*100))
+
+// 5. Write program  `rollDice` that simulates rolling two dice and returns their combined result.
+// Hint: Each dice roll should return a number between 1 and 6.
+
+const dice1 = Math.floor(Math.random()*6)+1
+const dice2 = Math.floor(Math.random()*6)+1
+
+console.log(dice1,dice2)
+console.log(dice1+dice2)
+
+// 6. What will be the result of the following code? Explain why.
+let randomVal = Math.random();

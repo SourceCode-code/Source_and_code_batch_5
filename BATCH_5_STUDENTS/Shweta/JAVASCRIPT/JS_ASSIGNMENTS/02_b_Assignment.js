6249d1ebbf5ae4d00db56fa11ebf9c401972981e
shwtabholankarwork <shweta.b.work7@gmail.com>
2026-03-22 19:44:54 +0530

    02_b_Assignment

---
+++ BATCH_5_STUDENTS/Shweta/JAVASCRIPT/JS_ASSIGNMENTS/02_b_Assignment.js
@@ -0,0 +1,11 @@
+//Create declare variable and assign string, boolean, undefined and null type of data
+
+let = a
+let = b
+let = c
+let = d
+
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+console.log(typeof d)

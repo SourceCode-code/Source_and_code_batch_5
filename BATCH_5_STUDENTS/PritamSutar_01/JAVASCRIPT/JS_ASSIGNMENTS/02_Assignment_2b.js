2d6dd2518f929d9ffaff13aed474ddb515dcc730
Rixty9 <atharvapandit84@gmail.com>
2026-03-22 20:54:25 +0530

    Assignment 3

---
+++ BATCH_5_STUDENTS/PritamSutar_01/JAVASCRIPT/JS_ASSIGNMENTS/02_Assignment_2b.js
@@ -0,0 +1,20 @@
+// Declaring variables and assign string, boolean, undefined and null data types
+
+let student = "shivani"
+
+let isStudent = true
+
+let yearswithworking
+
+let fruit = null
+
+
+
+// The JavaScript typeof operator to check different data types.
+console.log(typeof student)
+console.log(typeof isStudent)
+console.log(typeof years_Of_Experience)
+console.log(typeof fruit)
+
+////////////////////////////////
+//

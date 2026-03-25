4924f5330003da93826df2ac3cd0c5de9fdc5a90
kalyani <shindekalyani@87.gmail.com>
2026-03-25 20:22:22 +0530

    Assignment 04

---
+++ BATCH_5_STUDENTS/Kalyani/JAVASCRIPT/JS_ASSIGNMENTS/ASSIGN_04.js
@@ -0,0 +1,46 @@
+
+//Q1-ANS
+
+
+let greeting = "Hello World";
+console.log(typeof greeting);
+
+//Q2-ANS
+
+let price = 199.99
+console.log(Math.ceil(199.99))
+
+//Q3-ANS
+
+
+let name ="kalyani suraj sherkar"
+console.log(name.length) 
+
+//Q4-ANS
+let firstName = "kalyani"
+let middleName = "suraj"
+let lastName = "sherkar"
+
+console.log(firstName+" "+middleName +" "+lastName) 
+console.log(`${firstName} ${middleName} ${lastName}`)
+
+//Q5-ANS
+let language ="Javascript"
+console.log(language[4])
+
+//Q7-ANS
+let long = "jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj\
+hhhhhhhhhhhhhhhhhhhhhhhhhh"
+
+console.log(long)
+
+//Q8-ANS
+console.log("i\nam\nlearning\njavascript")
+console.log("its \"fun\" to learn javascript")
+
+
+//Q9-ANS
+let amount = 45.85
+console.log(Math.ceil(45.85))
+console.log(Math.floor(45.85))
+console.log(Math.round(45.85))

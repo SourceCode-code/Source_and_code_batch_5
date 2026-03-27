48150eaad0149d648f0893817ba676da398204b7
Dhananjay Ugwekar <dhananjayugwekar.work@gmil.com>
2026-03-27 16:49:47 +0530

    04_DU_Assignment_4

--- JAVASCRIPT/JAVASCRIPT_SESSION/05_DATATYPE_STRING_METHODS.js
+++ JAVASCRIPT/JAVASCRIPT_SESSION/05_DATATYPE_STRING_METHODS.js
@@ -89,3 +89,84 @@ console.log(trim_ex.trimStart())
 console.log(trim_ex.trimEnd().length)
 
 
+// =============================================
+// 5. subStr() 
+// ---------------------------------------------
+// 5. subStr() --> this method will get you a part of string and create a new string
+// subStr(starting index , number of character)
+
+let SUB_STR = "hello this is example of sub str for removeing some part of string"
+
+let HELLO_STR = SUB_STR.substr(0,10)
+console.log(HELLO_STR)
+
+
+// =============================================
+// 6. substring() 
+// ---------------------------------------------
+// 6. substring() -> this method will get you a part of string and create a new string
+ //substring(starting index , ending index ) // it will not include the ending index in the range
+
+ let SUB_STRING = "hello this is example of sub string for removeing some part of string"
+
+ let SUB_STRING_EX = SUB_STRING.substring(0,4)
+ console.log(SUB_STRING_EX)
+
+// =============================================
+// 7. includes() 
+// ---------------------------------------------
+//7. includes() --> this will check if a certain character or sequence of character are present in string
+// this will check that means output will be in --> Boolean (true and false)
+// THIS IS A CASE SENSTIVE METHOD
+
+let INCULDE_EX = "hello this is an example to show inculdes in string"
+
+console.log(INCULDE_EX.includes("Example"))
+
+// =============================================
+// 8. replace() 
+// ---------------------------------------------
+//replace(word that is to be replace , new word that will replace old) --> this method will replace some character in string 
+// NOTE :- it will only replace the first occurance of the word  
+let REPLACE_EX = "hello i am learning python and this topic string method in python"
+
+ console.log(REPLACE_EX.replace("python","js"))
+
+
+// =============================================
+// 9. replaceAll() 
+// ---------------------------------------------
+
+//replaceAll(word that is to be replace , new word that will replace old) --> this method will replace all character in string 
+// NOTE :- it will replace ALL the  occurance of the word  let REPLACE_EX = "hello i am learning python and this topic string method in python"
+let REPLACE_ALL_EX = "hello i am learning python and this topic string method in python"
+
+ console.log(REPLACE_ALL_EX.replaceAll("python","js"))
+
+
+ // IMP: -> remove all the space form the string given string
+
+ let REMOVE_SPACE = " HELLO i am learning replace all in js "
+
+ console.log(REMOVE_SPACE.replaceAll(" ",""))
+
+
+ // =============================================
+// 10. indexOf() 
+// ---------------------------------------------
+// 10. indexOf()  --> this will give you the index  of the given charcacter or the string
+// THIS WILL ONLY GIVE ME THE INDEX OF FIRST OCCURANCE
+
+let EX_index = "hello my name is sid"
+
+console.log(EX_index.indexOf("h"))
+console.log(EX_index.indexOf("e"))
+
+ // =============================================
+// 11. lastIndexOf() 
+// ---------------------------------------------
+// 11. lastIndexOf() --> this will give you the lastindex  of the given charcacter or the string
+// THIS WILL ONLY GIVE ME THE INDEX OF last OCCURANCE
+
+console.log(EX_index.lastIndexOf("h"))
+console.log(EX_index.lastIndexOf("e"))

d9ed1b5e0a66b39d9d87efbeb90f222a0203dd72
SourceCode-code <siddhantsourceandcode@gmail.com>
2026-04-13 21:53:48 +0530

    loops session

--- JAVASCRIPT/JAVASCRIPT_SESSION/10_loops.js
+++ JAVASCRIPT/JAVASCRIPT_SESSION/10_loops.js
@@ -95,10 +95,98 @@ for (let i = 1; i <= 5; i++) {
     let row = "";
     for (let j = 1; j <= i; j++) {
         row += "#"
-
         // row = "#"+"" // #
         // row = "#"+"#" //##
         // row = "##"+"#" //###
     }
     console.log(row)
 }
+
+
+// count the vowels in the string 
+
+let aplha_str = " hEllo my name is siddhant"
+// this string has 7 vowels
+let count = 0
+// vowels = "aeiou"
+
+for (let i = 0; i < aplha_str.length; i++) {
+    let char = aplha_str[i].toLowerCase()
+    if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
+        count++
+    }
+}
+
+console.log(`The above string has ${count} vowels`)
+
+
+
+// count the words in the following string 
+
+let word_string = "hello my name is siddhant and i am currently teaching js loops which are used to perform repativet action"
+
+let word_count = 1
+
+console.log(word_count)
+
+for (let j = 0; j < word_string.length; j++) {
+    if (word_string[j] === " ") {
+        word_count++
+    }
+
+
+}
+
+console.log(word_count)
+
+
+// complex logic 
+
+// in the given password check if the password is weak or strong 
+// condition for strong password ==> 
+// length should 8 characters 
+// it should have numbers  
+
+
+let pass = "1password"
+
+let hasNumber = false
+
+for (let i = 0; i < pass.length; i++) {
+    if (pass[i] >= "0" && pass[i] <= "9") {
+        hasNumber = true
+    }
+}
+
+if(pass.length>=8&&hasNumber){
+console.log("Strong Password")
+}
+else{
+    console.log("weaker password")
+}
+
+
+//Q count the total sum of numbers form 0 - 100
+
+let sum =0 
+for(let i =0;i<=100;i++){
+    // sum = sum+i
+    sum+=i
+
+}
+console.log(sum)
+
+
+
+//Q find how many number are  divisable by 3 and 5 in 0-200
+
+let divisor_count = 0
+
+for(let i =3 ;i<=200;i++){
+    if(i%3===0&& i%5 ===0){
+        divisor_count++
+    }
+}
+
+console.log(divisor_count)
+

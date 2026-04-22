73da5e4f8bfe1341560ba286426c225ab4daec4f
Ganesh <gdhage86@gmail.com>
2026-04-22 22:08:21 +0530

    Completed remaining 20 questions

--- BATCH_5_STUDENTS/Ganesh/JAVASCRIPT/JS_ASSIGNMENTS/10_b_Revision_Assignment.js
+++ BATCH_5_STUDENTS/Ganesh/JAVASCRIPT/JS_ASSIGNMENTS/10_b_Revision_Assignment.js
@@ -155,47 +155,128 @@ console.log(newString);
 // =====================================================
 
 // Q21. Given string = "  Diwali Celebration  ", remove extra spaces and print clean version.
+console.log("  Diwali Celebration  ".trim());
+
 // Q22. Extract the last 4 characters of any given string.
+let str = "Diwali Celebration";
+console.log(str.substring(str.length - 4));
+
 // Q23. Convert “Javascript is fun” → “JAVASCRIPT IS FUN” → “javascript is fun”.
+let str2 = "Javascript is fun";
+console.log(str2.toUpperCase());
+console.log(str2.toLowerCase());
+
 // Q24. Replace all occurrences of “light” with “spark” in “Festival of lights brings light”.
+let str3 = "Festival of lights brings light";
+console.log(str3.replaceAll("light", "spark"));
+
 // Q25. Find if a string starts with “Happy” and ends with “Diwali”.
+let str4 = "Happy Festival of lights brings light, so Diwali";
+str4.startsWith("Happy") && str4.endsWith("Diwali")
+  ? console.log(`given string starts with Happy and ends with Diwali`)
+  : console.log("given string does not start with Happy and ends with Diwali");
 
 // =====================================================
 // 5. CONVERSION (NUMBER <-> STRING)
 // =====================================================
 
 // Q26. Convert number 2025 into a string and print its type.
+let nn =2025;
+let nm = nn.toString();
+console.log(typeof nm);
+
 // Q27. Convert string “99.9” to number and round it.
+console.log(Math.round(Number("99.9")));
+
 // Q28. Add number + string and explain result for (5 + "5") and (5 - "5").
+console.log((5 +"5")); // + sign acts as concatination
+console.log( (5-"5")); // - sign will convert string to number 
+
 // Q29. Convert true → 1 and false → 0 using Number().
+console.log(Number(true));
+console.log(Number(false));
+
 // Q30. Ask: what happens when you do String(10 + 5) vs String(10) + String(5)?
+console.log(String(10 +5)); //first operation is addition and then convert to string
+console.log(String(10) + String(5)); //first operation is conversion then addition os 2 string
 
 // =====================================================
 // 6. OPERATORS (LOGICAL + ARITHMETIC + TERNARY)
 // =====================================================
 
 // Q31. Calculate and print: ((10 + 3) ** 2) / 13.
+let ans = (10 + 3) ** 2 / 13;
+console.log(ans);
+
 // Q32. Create a program that checks if a number is divisible by both 3 and 5.
+let num32 = 14;
+num32 % 3 === 0 && num32 % 5 === 0
+  ? console.log(`${num32} is divisible by 3 and 5`)
+  : console.log(`${num32} is not divisible by 3 and 5`);
+
 // Q33. Use logical operators to check if a student’s age is between 18–25 and from “Pune”.
+age = 27;
+let place = "Pune";
+(age <= 25 && age >= 18 && place === "Pune")
+  ? console.log(`Both conditions are true`)
+  : console.log(`one or more conditions are false`);
+
 // Q34. Use ternary operator to check if given marks >= 40 → “Pass” else “Fail”.
+givenMark = 45;
+givenMark >=40?console.log(`pass`):console.log("fail");
+
 // Q35. Find the output of: console.log(5 + 3 * 2 ** 2) and explain order of evaluation.
+console.log(5 + 3 * 2 ** 2)
+// order of operation = 2**2 then 4*3 then 12+5
 
 // =====================================================
 // 7. DATE (REAL-TIME APPLICATIONS)
 // =====================================================
 
 // Q36. Print today’s date in format “DD-MM-YYYY”.
+let dateObject = new Date();
+let dd = dateObject.toLocaleString("en-IN", { day: "2-digit" });
+let mm = dateObject.toLocaleString("en-IN", { month: "2-digit" });
+console.log(`${dd}-${mm}-${dateObject.getFullYear()}`);
+
 // Q37. Print current time in format “HH:MM:SS”.
+let hh = dateObject.toLocaleString("en-IN", { hour: "2-digit", hour12: false });
+mm = dateObject.toLocaleString("en-IN", { minute: "2-digit" });
+let ss = dateObject.toLocaleString("en-IN", { second: "2-digit" });
+console.log(`${hh}:${mm}:${ss}`);
+
 // Q38. Create a Date for “24 Oct 2025” and find which day it falls on.
-// Q39. Calculate how many days are left for Diwali 2025 (use Date difference).
+let newDate = new Date("24 oct 2025");
+console.log(newDate.toLocaleString("en-IN", { weekday: "long" }));
+
+// Q39. Calculate how many days are left for Diwali 2026 (use Date difference).
+let diwaliDate = new Date("5 nov 2026");
+let diff = Math.floor((diwaliDate - dateObject)/(1000*60*60*24));
+console.log(diff);
 // Q40. Print “Good Morning / Good Afternoon / Good Evening” based on current hour.
+console.log(hh);
+switch (true) {
+  case hh >= 5 && hh < 12:
+    console.log(`Good Moring`);
+    break;
+  case hh >= 12 && hh < 18:
+    console.log(`Good Afternoon`);
+    break;
+  case hh >= 18 && hh < 24:
+    console.log(`Good Evening`);
+    break;
+  default:
+    console.log(`Invalid input`);
+}
 
 /* ------------------------------------------------------------------
 🌟 BONUS CHALLENGE (Optional)
 Create a program called “Diwali Countdown”.
 It should print today’s date and how many days left until Diwali.
 Then display “🪔 Get Ready to Celebrate!” if less than 10 days remain.
------------------------------------------------------------------- 
+------------------------------------------------------------------ */
+diff <=10?console.log(`🪔 Get Ready to Celebrate!`):console.log(`${diff} to Celebrate! 🪔`);
+/*
 🎉 Remember: Programming is not about writing code —
 it’s about breaking problems into logic
 */

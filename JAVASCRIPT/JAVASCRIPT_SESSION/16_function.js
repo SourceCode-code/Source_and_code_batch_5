7608d3315e8960c906db5ec8d45a0cdd4e720d51
SourceCode-code <siddhantsourceandcode@gmail.com>
2026-05-05 21:47:22 +0530

    function

---
+++ JAVASCRIPT/JAVASCRIPT_SESSION/16_function.js
@@ -0,0 +1,78 @@
+
+// WHAT IS VARIABLE ?
+
+/** 
+ * ITS IS CONATINER WHICH IS USED TO STORE DATA 
+ * 
+ */
+let x = 10 // it shows that the value 10 is placed in container called as x 
+let y = 20
+
+
+console.log(x + y)
+console.log(x - y)
+console.log(x * y)
+console.log(x / y)
+console.log(x % y)
+console.log(x ** y)
+
+// i need to perform same opertaion for the given varaible 
+let z = 100
+let u = 50
+
+//  WHAT IS FUNCTION ?
+//FUNCTION IS CONATINER WHICH IS USED TO CODE DATA TO MAKE YOUR CODE DYNAMIC AND RESUABLE
+
+/**
+ * 
+ * SYNTAX OF FUNCTIONS 
+ * 
+ * function functionname(parameter){
+ * logical code
+ * }
+ * 
+ *HOW TO CALL A FUNCTION
+ *  NAMEOFFUNCTION(PARAMETER)
+ */
+// CREATE A FUNCTION TO WRITE ARTHIMATIC OPERATIONS
+function arthmatic_op(num1, num2) {
+    console.log(num1 + num2)
+    console.log(num1 - num2)
+    console.log(num1 * num2)
+    console.log(num1 / num2)
+    console.log(num1 % num2)
+    console.log(num1 ** num2)
+}
+arthmatic_op(20, 30)
+console.log("___________________")
+arthmatic_op(100, 50)
+
+// ADVANATAGES OF FUNCTIONS
+/**
+ * 1 makes code reusable (saves time)
+ * 2 keep your code clean and dynamic
+ * 3 avoid unnessacry dupliaction
+ * 4 makes its easy to understand the code and managed
+ */
+
+
+// TYPES OF FUNCTIONS 
+
+// THERE ARE 2 MAJOR CLASSIFICATION TYPES OF FUNCTION
+
+//1 ON BASIS ON OF WRITING IT 
+/**
+ * 1 DELECRATIVE FUNCTION
+ * 2 EXPERSSION TYPE FUNCTION
+ * 3 ARROW TYPE FUNCTION
+ */
+
+
+//2 ON BASIS OF PARAMETER
+
+/**
+ * 1 WITHOUT PARAMTER WITHOUT RETURN TYPE
+ * 2 WITH PARAMTER WITHOUT RETURN TYPE
+ * 3 WITH PARAMTER WITH RETURN TYPE
+ */
+

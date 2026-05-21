b0d9ad53982e8b8117547f0a996d2d9753bc033e
SourceCode-code <siddhantsourceandcode@gmail.com>
2026-05-21 21:58:44 +0530

    high order

--- JAVASCRIPT/JAVASCRIPT_SESSION/21_High_order_functions.js
+++ JAVASCRIPT/JAVASCRIPT_SESSION/21_High_order_functions.js
@@ -9,8 +9,8 @@
 
 // first function 
 
-const callback = (n)=>{
-    return n**2
+const callback = (n) => {
+    return n ** 2
 }
 
 
@@ -18,30 +18,30 @@ console.log(callback(2))
 
 // callback or second function 
 
-function get_cube(callback,n){
-    return callback(n)*n
+function get_cube(callback, n) {
+    return callback(n) * n
 }
 
-console.log(get_cube(callback,2)) //8
+console.log(get_cube(callback, 2)) //8
 
 
 
 // create a callback function for assignment
 
-function completing_assignemnt(){
+function completing_assignemnt() {
     console.log("i am completed my assignemnt")
 }
 
 
-function notdone(){
+function notdone() {
     console.log("............not done")
 }
 
 
-function rasing_pr(callback){
-     
+function rasing_pr(callback) {
+
     console.log("rasied the pr for assginemnt");
-   callback()
+    callback()
 }
 
 
@@ -55,12 +55,12 @@ rasing_pr(notdone)
 //example 
 
 
-const first=(n)=>{
-    const second=(m)=>{
-        const thrid =(l)=>{
-            return n+m+l
+const first = (n) => {
+    const second = (m) => {
+        const thrid = (l) => {
+            return n + m + l
         }
-       return thrid 
+        return thrid
     }
     return second
 }
@@ -75,3 +75,70 @@ console.log(first(1)(2)(3))
 // 2 setTimeout() --> to dealy the output of function by a certain time is called setTimeout 
 
 //SYNC -- ASYNC 
+
+
+//setInterval()--> the gobal method which takes a callback function as a parameter and duratio as a parameter
+
+// SYNTAX 
+
+/**
+ * 
+ * setInvertal(callback(),timeinterval)
+ */
+
+
+// setInterval(function HEllo(){
+//     console.log("hello")
+// },2000)
+
+
+// example 2 
+
+// function Welcome(){
+//     console.log("welcome to advance js")
+// }
+
+// setInterval(Welcome,1000)
+
+
+// setTimeout() -->  this method is used to trigger a function by the given amount of delay 
+// in this we have first as callback and second time of delay 
+
+// syntax setTimeout(callback,delay)
+
+// setTimeout(() => {
+//     console.log("this will print after 5 sec")
+// }, 5000);
+
+
+
+
+// hybrid problem 
+
+// creation a script where a function is trigger every 3 sec saying welcome to js 
+// after 10 sec it gets completed 
+
+let interval = setInterval(()=>{
+    console.log("welcome to world js")
+},3000)
+
+setTimeout(()=>{
+ clearInterval(interval)
+  console.log("program has eneded")
+},10000)
+
+
+// 
+
+console.log("test 123")
+
+
+// SYNCHROUSING ISSUE 
+
+// ASYNCHROUS ISSUE 
+
+
+// Callback --> 
+// promsie 
+// async and await 
+

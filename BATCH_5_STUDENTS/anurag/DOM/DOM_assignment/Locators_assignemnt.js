b5fdd9860ac91894611656b2bde3638d042abbd0
anuragdeshpandework <anuragdeshpande.work@gmail.com>
2026-06-16 09:13:31 +0530

    locators assignment added

---
+++ BATCH_5_STUDENTS/anurag/DOM/DOM_assignment/Locators_assignemnt.js
@@ -0,0 +1,42 @@
+/** 
+ * flipkart.com
+ * 
+ * <input class="nw1UBF v1zwn25" style="color:#3d3d3dff" 
+ * type="text" title="Search for Products, Brands and More" 
+ * autocomplete="off" placeholder="Search for Products, 
+ * Brands and More" name="q" value="">
+ * 
+ * Locators
+ * class="nw1UBF v1zwn25"
+ * 
+ *   SELECTOR-->.nw1UBF
+ *   SELECTOR-->[class="nw1UBF v1zwn25"]
+ *  
+ * 
+ * 
+ * amazon.in
+ * 
+ * <input type="text" id="twotabsearchtextbox" 
+ * value="" name="field-keywords" autocomplete="off" 
+ * placeholder="Search Amazon.in" class="nav-input nav-progressive-attribute" 
+ * dir="auto" tabindex="0" aria-label="Search Amazon.in" role="searchbox" 
+ * aria-autocomplete="list" aria-controls="sac-autocomplete-results-container" 
+ * aria-expanded="true" aria-haspopup="grid" spellcheck="false">
+ * 
+ * Locators
+ * ID locator 
+ * SELCTOR-->#twotabsearchtextbox
+ * SELCTOR-->[id="twotabsearchtextbox"]
+ * 
+ * class locator
+ * SELCTOR-->.nav-input nav-progressive-attribute
+ * SELCTOR-->[class="nav-input nav-progressive-attribute"]
+ * 
+ * role locator
+ * SELCTOR-->[role="searchbox"]
+ * 
+ * 
+ * 
+ * 
+ * 
+ */

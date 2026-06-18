fe5efcd9a824be1a8b3a22828e0f51fb91404eb6
shivani <shvanikshirsagar2204@gmail.com>
2026-06-18 16:38:48 +0530

    dom-tableselector

--- BATCH_5_STUDENTS/shivani/JAVASCRIPT/JS_ASSIGNMENTS/27_tableshivaniassignment.js
+++ BATCH_5_STUDENTS/shivani/JAVASCRIPT/JS_ASSIGNMENTS/27_tableshivaniassignment.js
@@ -1,1 +1,44 @@
 // table assignment
+
+/*ipl table -
+
+1.RCB 
+======
+name selector ==>table[class="Jzru1c"] tbody tr:nth-child(2) td:nth-child(3) div:nth-child(1) div:nth-child(1) span[class="ellipsisize hsKSJe"]
+pts selector==>table[class="Jzru1c"] tbody tr:nth-child(2) td:nth-child(8) 
+NRR selector ==>table[class="Jzru1c"] tbody tr:nth-child(2) td:nth-child(7) div[class="Er8EJ"]
+
+2.RR
+=====
+name selector==>table[class="Jzru1c"] tbody tr:nth-child(5) td:nth-child(3) .e6E1Yd div[class="imso-hide-overflow C1gYkd"] span[class="ellipsisize hsKSJe"]
+image selector ==>table[class="Jzru1c"] tbody tr:nth-child(5) td:nth-child(2) div:nth-child(3) span[class="iU5t0d b8Oacc fz3Rib"] div:nth-child(1) #dimg_jbczarzuMdjTwcsPuqKzwQM_9
+
+*/
+
+
+/*ipl player run table 2026
+===========================
+
+1. Virat Kohli
+===============
+team selector ==>table[class="NRefec"] tbody tr:nth-child(5) td:nth-child(3)
+runs selector ==>table[class="NRefec"] tbody tr:nth-child(5) td:nth-child(5) strong[class="Yjhzub"]
+
+
+2.Ishan Kishan
+==============
+Strike Rate selector ==>table[class="NRefec"] tbody tr:nth-child(7) td:nth-child(7) 
+
+3.Abhishek Sharma
+=================
+name selector==> table[class="NRefec"] tbody tr:nth-child(9) td:nth-child(2)
+*/
+
+
+/*NSE stock table
+=================
+1. Reliance Industries
+=================
+name selector ==>table[id="equityStockTable"] tbody tr:nth-child(3) td:nth-child(1) a[href="/get-quotes/equity?symbol=RELIANCE"]
+volume selector ==>#equityStockTable tbody tr:nth-child(3) td:nth-child(10)
+*/

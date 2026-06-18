71f4d568a9e9901f7991c31557f6567699146c19
Rasane Anurag Prashant <125312282+anuragrasane@users.noreply.github.com>
2026-06-18 19:34:39 +0530

    css selector assignment

---
+++ BATCH_5_STUDENTS/Anurag Rasane/selector_Assignment.js
@@ -0,0 +1,42 @@
+//Website https://jobviareferral.com/
+/*
+[placeholder="Search jobs...."]
+[id="menu-item-4086"]
+.gb-element-541b522e
+[class="mobile-menu"]
+[class="menu-bar-item"]
+
+amazon.in
+#navBackToTop
+https://www.amazon.in › home
+[id="nav-logo"]
+[placeholder="Search Amazon.in"]
+[class="a-cardui-header"]
+[class="navFooterDescItem_heading"]
+[class="nav-logo-base nav-sprite"]
+
+
+
+https://findmyjobss.com/infosys-recruitment-2026-3/
+[id="ast-scroll-top"]
+[title="Peroptyx Recruitment 2026 for Data Analyst | Work From Home Opportunity"]
+[id="comment"]
+[value="Post Comment"]
+textarea
+
+https://www.flipkart.com/
+
+a[aria-label="Payments"]
+div[class="PGj60j"]
+[class="_58bkzqcd _3n8fna1co _3n8fna10j _3n8fnaod _3n8fna1 _3n8fnac7 _58bkzq2 _1i2djtb9 _1i2djt0"]
+[alt="Help Center"]
+[class="nIDtl3 b1mZeO"]
+
+https://vegamovie.sl/
+#header-category
+.site-header.header-default.site-header-control
+[title="Dual Audio [Hindi]"]
+[placeholder="Search..."]
+li[title="Dual Audio [Hindi]"]
+
+*/

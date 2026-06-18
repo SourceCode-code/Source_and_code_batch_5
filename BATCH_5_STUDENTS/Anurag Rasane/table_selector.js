8be09d8042ce3918cac2769b5539015d55d2902a
Rasane Anurag Prashant <125312282+anuragrasane@users.noreply.github.com>
2026-06-18 21:30:46 +0530

    CSS Selector

---
+++ BATCH_5_STUDENTS/Anurag Rasane/table_selector.js
@@ -0,0 +1,26 @@
+/*
+Table selector
+https://www.iplt20.com/matches/points-table
+
+table tbody tr:nth-child(8)
+
+
+https://www.iplt20.com/stats/2026
+
+tbody tr a div[class="st-ply-name ng-binding"]
+[class="RR_0"]
+
+[class="RR_0"] [class="st-ply-name ng-binding"]
+
+
+
+https://indianexpress.com/section/sports/ipl/stats/most-runs/
+
+
+
+https://www.fifa.com/en/tournaments/mens/worldcup/canadamexicousa2026/standings
+
+https://www.nseindia.com/index-tracker/NIFTY%2050
+
+https://agritech.tnau.ac.in/agriculture/agri_soil_soilratingchart.html
+*/

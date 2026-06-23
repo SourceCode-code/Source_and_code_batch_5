3fd31cf1ef87b2c3e66732f1d7d29d75c0694e74
Rasane Anurag Prashant <125312282+anuragrasane@users.noreply.github.com>
2026-06-23 20:09:22 +0530

    Xpath Selectors

---
+++ BATCH_5_STUDENTS/Anurag Rasane/xpath_Selector.js
@@ -0,0 +1,21 @@
+https://findmyjobss.com/
+//a[@class="next page-numbers"]
+//div[@id="ast-scroll-top"]
+//li[@id="menu-item-2214"]
+//li[@id="menu-item-45"]
+//span[@class="site-logo-img"]
+
+https://awesomeqa.com/ui/
+//div[@class="swiper-button-next"]
+//div[@id="cart"]
+//i[@class="fa fa-user"]
+//div[@id="logo"]
+//input[@name="search"]
+
+
+https://www.microsoft.com/en-in
+//a[@class="uhf-microsoft-logo"]
+//span[@class="uhf-search-text"]
+//a[@data-bi-cn="Explore AI solutions "]
+//a[@data-bi-cn="Back to top"]
+//a[@id="globalazure"]

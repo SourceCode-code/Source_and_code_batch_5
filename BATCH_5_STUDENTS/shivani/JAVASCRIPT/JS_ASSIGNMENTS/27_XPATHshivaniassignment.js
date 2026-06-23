71edaf1275273f7082302988fc74903d29098118
shivani <shvanikshirsagar2204@gmail.com>
2026-06-23 14:38:22 +0530

    Xpath

---
+++ BATCH_5_STUDENTS/shivani/JAVASCRIPT/JS_ASSIGNMENTS/27_XPATHshivaniassignment.js
@@ -0,0 +1,56 @@
+/*The Internet
+https://the-internet.herokuapp.com/
+1. heading 
+//h1[@class="heading"]
+
+2. Fork me on GitHub image
+//img[@alt="Fork me on GitHub"]
+
+================================================================================
+
+Automation Practice
+http://automationpractice.com/index.php
+1. inmotion hoisting image
+//img[@border="0"]
+
+================================================================================
+
+Sauce Demo
+https://www.saucedemo.com/
+1.username input field
+//input[@id="user-name"]
+
+
+2. password inpute field
+//input[@name="password"]
+
+3. login button
+//input[@class="submit-button btn_action"]
+
+================================================================================
+
+Practice Automation Testing
+http://practice.automationtesting.in/
+
+1.Ver más button
+//div[@class="goog-rtopics"]
+
+ 2. add to basket button
+ //a[@data-product_id="160"]
+
+ 3.thinking in HTML image
+ //img[@title="Thinking in HTML"]
+
+ ================================================================================
+
+ToolsQA Demo Site
+https://demoqa.com/
+
+1. join now button
+//img[@class="banner-image"]
+
+//img[@alt="Selenium Online Training"]
+ ================================================================================
+
+
+*/

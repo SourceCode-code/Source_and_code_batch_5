113bed2c314b5da5ee012337ec8d28335941f534
SourceCode-code <siddhantsourceandcode@gmail.com>
2026-06-30 21:20:13 +0530

    playwright installtion

---
+++ PLAYWRIGHT_MAIN/tests/01_Basic_intro.spec.js
@@ -0,0 +1,11 @@
+const {test,expect}= require("@playwright/test")
+//This line is used to import all the smart suggestion for test steps and asseration 
+
+
+// ACTION/COMMAND        -->THE ACTION PERFOMED BY THE USER
+// ASSEARTION/VALIDATION -->TO VERIFY THE EXPECTED OUTPUT OF THE ACTION
+
+// TESTCASE  --> SINGLE TEST (Step by step instruction to get intended result)
+// TESTSUITE --> COMBINATION OF SIMILAR TESTCASE (Test suits are collection of test cases)
+
+

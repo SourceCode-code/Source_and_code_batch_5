84a10edc67d9ddc0d38c1a3e1d3f73b64d252295
udaynpatil5353 <udaynpatil5353@gmail.com>
2026-07-02 21:05:46 +0530

    new

---
+++ STUDENT_PLAYWRIGHT/tests/UDAY_PLAYWRIGHT/01_test.spec.js
@@ -0,0 +1,18 @@
+
+const { test, expect } = require("@playwright/test")
+test('To verify login functionality', async ({ browser }) => {
+
+    const Context = await browser.newContext();
+    const page = await Context.newPage();
+
+    await page.goto('https://practicetestautomation.com/practice-test-login/');
+    
+    await expect(page).toHaveTitle("Test Login | Practice Test Automation");
+
+    await page.locator('[id="username"]').fill('student');
+    await page.locator('[id="password"]').fill('Password123');
+    await page.locator('[id="submit"]').click();
+
+    await expect(page.locator('[class="post-title"]')).toHaveText('Logged In Successfully');
+    
+});

711011ec5db5b59274bb43312765723d94d600f1
SourceCode-code <siddhantsourceandcode@gmail.com>
2026-07-03 21:07:28 +0530

    playwright test -1

--- PLAYWRIGHT_MAIN/tests/01_Basic_intro.spec.js
+++ PLAYWRIGHT_MAIN/tests/01_Basic_intro.spec.js
@@ -23,6 +23,36 @@ test("verify successfull filling of the contact us form ", async ({ browser }) =
     await page.goto("https://webdriveruniversity.com/Contact-Us/contactus.html")
     // asseration to verify title of the page 
     await expect(page).toHaveTitle("WebDriver | Contact Us")
-    
+    // fill firstname
+    //page.locator() method --> it is used to find element  
+    //.fill() --> it is used to fill data in to inputbox
+    await page.locator('[name="first_name"]').fill("ABCD")
+    //.toHaveValue() --> to verify the temp value filled in input value
+    await expect(page.locator('[name="first_name"]')).toHaveValue("ABCD")
+    //
+    await page.locator('//input[@name="last_name"]').fill("EFGH")
+    //.toHaveValue() --> to verify the temp value filled in input value
+    await expect(page.locator('//input[@name="last_name"]')).toHaveValue("EFGH")
+    //
+    await page.locator('[name="email"]').fill("EFGH@gmail.com")
+    //.toHaveValue() --> to verify the temp value filled in input value
+    await expect(page.locator('[name="email"]')).toHaveValue("EFGH@gmail.com")
+   await page.locator('[name="message"]').fill("hello")
+    //.toHaveValue() --> to verify the temp value filled in input value
+    await expect(page.locator('[name="message"]')).toHaveValue("hello")
+
+//check if button is enabled 
+// .toBeEnabled() --> checks if the button is clickable
+//.toBeDisabled() --> checks if button is not clickable
+await expect(page.locator('input[type="submit"]')).toBeEnabled()
+
+//.click --> used to click on elements 
+await page.locator('input[type="submit"]').click()
+
+// .toHaveText(same text ) --> check  same if hardcoded text is present
+//.toContainText("half text") --> checks if text is present input is partical
+// above both methods are case senstive 
+await expect(page.locator("h1")).toHaveText("Thank You for your Message!")
+await expect(page.locator("h1")).toContainText("Thank You")
 
 })

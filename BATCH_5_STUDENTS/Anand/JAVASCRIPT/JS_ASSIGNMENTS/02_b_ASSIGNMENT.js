b78adc747c5b069be494060d5b0b00725993384d
Anand Alnkar <anandalankar.work@gmail.com>
2026-07-17 21:17:58 +0530

    Complete 02_b_Assignment

---
+++ BATCH_5_STUDENTS/Anand/JAVASCRIPT/JS_ASSIGNMENTS/02_b_ASSIGNMENT.js
@@ -0,0 +1,32 @@
+// Create declare variables and assign string, boolean, undefined and null data types
+
+//1_String
+let Name = `Anand`
+console.log(typeof Name)
+
+//2_Number
+let Age = 32
+console.log(typeof Age)
+
+//3_Boolean
+let Boolen = true
+console.log(typeof Boolen)
+
+//4_Undefined
+let Surname
+console.log(typeof Surname)
+
+//5_Null
+let Universe = null
+console.log(typeof Universe)
+
+
+/**
+ * Checking data types using typeof operator
+ * Results:
+ * string
+ * number
+ * boolean
+ * undefined
+ * object
+ */

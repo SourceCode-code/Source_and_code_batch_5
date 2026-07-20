49b432de43b932ddae0ad2e6023f5dda9fa359d9
anuragdeshpandework <anuragdeshpande.work@gmail.com>
2026-07-20 20:58:49 +0530

    added the assignemnt

--- BATCH_5_STUDENTS/anurag/JAVASCRIPT/JS_ASSIGNMENTS/16_anurag_assignment.js
+++ BATCH_5_STUDENTS/anurag/JAVASCRIPT/JS_ASSIGNMENTS/16_anurag_assignment.js
@@ -202,3 +202,115 @@ console.log(findMax(0, 10, 5))
 console.log(findMax(0, -10, -2))
 
 console.log("=====================================")
+
+/**
+ * Exercises: Level 2
+
+1 Linear equation is calculated as follows: ax + by + c = 0. Write a function which calculates value of a linear equation, solveLinEquation.
+
+2 Quadratic equation is calculated as follows: ax2 + bx + c = 0. Write a function which calculates value or values of a quadratic equation, solveQuadEquation.
+
+console.log(solveQuadratic()) // {0}
+console.log(solveQuadratic(1, 4, 4)) // {-2}
+console.log(solveQuadratic(1, -1, -2)) // {2, -1}
+console.log(solveQuadratic(1, 7, 12)) // {-3, -4}
+console.log(solveQuadratic(1, 0, -4)) //{2, -2}
+console.log(solveQuadratic(1, -1, 0)) //{1, 0}
+
+3 Declare a function name printArray. It takes array as a parameter and it prints out each value of the array.
+
+4 Write a function name showDateTime which shows time in this format: 08/01/2020 04:08 using the Date object.
+
+showDateTime()
+08/01/2020 04:08
+
+5 Declare a function name swapValues. This function swaps value of x to y.
+
+swapValues(3, 4) // x => 4, y=>3
+swapValues(4, 5) // x = 5, y = 4
+
+6 Declare a function name reverseArray. It takes array as a parameter and it returns the reverse of the array (don't use method).
+
+console.log(reverseArray([1, 2, 3, 4, 5]))
+//[5, 4, 3, 2, 1]
+console.log(reverseArray(['A', 'B', 'C']))
+//['C', 'B', 'A']
+
+7 Declare a function name capitalizeArray. It takes array as a parameter and it returns the - capitalizedarray.
+
+8 Declare a function name addItem. It takes an item parameter and it returns an array after adding the item
+
+9 Declare a function name removeItem. It takes an index parameter and it returns an array after removing an item
+
+10 Declare a function name sumOfNumbers. It takes a number parameter and it adds all the numbers in that range.
+
+11 Declare a function name sumOfOdds. It takes a number parameter and it adds all the odd numbers in that - range.
+
+12 Declare a function name sumOfEven. It takes a number parameter and it adds all the even numbers in that - range.
+
+13 Declare a function name evensAndOdds . It takes a positive integer as parameter and it counts number of evens and odds in the number.
+
+evensAndOdds(100);
+The number of odds are 50.
+The number of evens are 51.
+
+14 Write a function which takes any number of arguments and return the sum of the arguments
+
+sum(1, 2, 3) // -> 6
+sum(1, 2, 3, 4) // -> 10
+
+15 Writ a function which generates a randomUserIp.
+
+16 Write a function which generates a randomMacAddress
+
+17 Declare a function name randomHexaNumberGenerator. When this function is called it generates a random hexadecimal number. The function return the hexadecimal number.
+
+console.log(randomHexaNumberGenerator());
+'#ee33df'
+
+18 Declare a function name userIdGenerator. When this function is called it generates seven character id. The function return the id.
+
+console.log(userIdGenerator());
+41XTDbE
+
+ */
+
+console.log("=====================================")
+console.log("level 2 assignment")
+
+//1 Linear equation is calculated as follows: ax + by + c = 0. Write a function which calculates value of a linear equation, solveLinEquation.
+console.log("linear equation")
+function solveLinearEquation(a, b, c, x, y){
+    // solving the linear equation ax + by + c = 0
+    return a * x + b * y + c;
+
+}
+console.log(solveLinearEquation(2, 3, 4, 1, 2)); // Output: 12
+console.log(solveLinearEquation(1, -1, 0, 3, 3));
+
+console.log("=====================================")
+
+//2 Quadratic equation is calculated as follows: ax2 + bx + c = 0. Write a function which calculates value or values of a quadratic equation, solveQuadEquation.
+console.log("quadratic equation")
+function solveQuadratic(a, b, c){
+    return a * x * x + b * x + c;
+
+}
+console.log(solveQuadratic(1, -3, 2)); // Output: 0
+console.log(solveQuadratic(1, 2, 1)); // Output: 0
+console.log(solveQuadratic(1, 0, -4)); // Output: 0
+console.log(solveQuadratic(1, -1, 0)); // Output: 0
+
+//3 Declare a function name printArray. It takes array as a parameter and it prints out each value of the array.
+console.log("print array")
+function printArray(arr){
+    for(let i=0; i < arr.length; i++){
+        console.log(arr[i]);
+    }
+}
+console.log(printArray([1, 2, 3, 4, 5]));
+
+// 4 write a function name showDateTime which shows time in this format: 08/01/2020 04:08 using the Date object.
+function showDateTime() {
+    const now = new Date();
+    const day = String

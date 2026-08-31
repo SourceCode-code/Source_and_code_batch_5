d7dff4027966e67a66a0ede55ed805cff3e81a83
SourceCode-code <siddhantsourceandcode@gmail.com>
2026-08-31 21:00:23 +0530

    message

---
+++ PLAYWRIGHT_MAIN/tests/11_API_TEST.spec.js
@@ -0,0 +1,99 @@
+// API ->APPLICATION PROGARMING INTERFACE
+
+//WEB APPLICATION -> 
+//FRONT END  -> WEBSITE UI THAT YOU SEE
+//BACKEND -> THE ACTION THAT YOU GET AFTER GIVENING REACTION OT WEBPAGE 
+//SERVER -> PLACED WHERE DATA IS STORED 
+
+
+// TECHINCAL ROLES 
+
+/**
+ * FRONT END DEVELPER
+ * BACKEND DEVELOPER 
+ * DATA BASE -> DATA BASE ADMINISTOR 
+ * DEVOPS ENGINNER 
+ * TESTOR -> MANUUAL 
+ * //API TESTOR -> API TEST
+ * //AUTOMATION -> UI AUTOMATION AS WELL API AUTOMATION
+ * 
+ */
+
+
+// API -> API IS THE COMMUNICATION LINK BETWEEN FRONT END AND THE SERVER
+
+// example is hotel kitchen
+
+/** 
+ * 
+ * in hotel 
+ * 
+ * 
+ * customer who order a dish                                            as user you perform action on ui 
+ * 
+ * 
+ * WAITER --> one who connect the customer and chef                      api will send the message to server 
+ * 
+ * 
+ * chef makes the dish                                                  gets the correct response and send it back to user with api 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+// there 2 types of apis 
+
+// 1 SOAP ->( THIS IS OLDER SERVICE) -> XML 
+// 2 REST APIS -> THIS IS NEW GEN SERVICE -> JSON(98%)
+// 3 PROGEST GRES ->
+// 4 GARFQL ->
+
+
+//SOAP -> SOAP UI 
+//REST -> POSTMAN , SWAGER 
+
+// REST SERIVESCES -> 
+/**
+ * GET  -> IT GETS ALL THE DATA/ INFORMATION
+ * POST -> CREATE IT NEW INFORMATION (CREATING A NEW ACCOUNT)
+ * PUT  -> UPDATE THE CREATED DATA  
+ * UPDATE -> UPDATE THE CREATED DATA  
+ * PATCH -> UPDATE THE CREATED DATA  
+ * DELETE --> DELETE ALL THE DATA 
+ */
+
+
+// SOME TERMS :- 
+
+// IF HAVE A SINGLE API TEST --> API REQUEST 
+// IF YOU HAVE A MULTIPLE API TO TEST --> COLLECTION
+
+
+//  SAMPLE OF XML DATA 
+
+// <?xml version="1.0" encoding="UTF-8"?>
+
+// <student>
+//     <id>101</id>
+//     <name>Rahul Sharma</name>
+//     <age>20</age>
+//     <course>Computer Science</course>
+//     <email>rahul@example.com</email>
+// </student>
+
+//JSON
+
+// {
+//   "student": {
+//     "id": 101,
+//     "name": "Rahul Sharma",
+//     "age": 20,
+//     "course": "Computer Science",
+//     "email": "rahul@example.com"
+//   }
+// }
+
+
+
